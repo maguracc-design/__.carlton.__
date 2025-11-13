eac7185a64a3227e43e09f883209f85ce9e63ee8
maguracc-design <maguracc@gmail.com>
2025-11-13 19:00:01 +0300

    Add files via upload
    My first project in C++, just a basic beginner calculator

---
+++ My first project(Calculator).cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double number1, number2;
+    string operation;
+    cout << "Enter first number: ";
+    cin >> number1;
+    cout << "Enter operator (+ , _, -, * , ^): ";
+    cin >> operation;
+    cout << "Enter second number: ";
+    cin >> number2;
+
+    if (operation == "+")
+    {
+        cout << "Answer is: " << number1 + number2;
+    }
+    if (operation == "-")
+    {
+        cout << "Answer is: " << number1 - number2;
+    }
+    if (operation == "/")
+    {
+        cout << "Answer is: " << number1 / number2;
+    }
+    if (operation == "^")
+    {
+        cout << "Answer is: " << pow(number1,number2);
+    }
+    else
+    {
+        cout << "Syntax Erro: ";
+    }
+    
+}
+
